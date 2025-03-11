070bec7b73c2544bc24ff36c34c0c08b2e030a43
GiovannaBeathryce <giovannabeathryce.pso@gmail.com>
2025-03-11 18:16:20 -0300

    search function implementation

--- galery/views.py
+++ galery/views.py
@@ -9,3 +9,13 @@ def index(request):
 def image(request, photo_id):
     photography = get_object_or_404(Photography, pk=photo_id)
     return render(request, 'galery/imagem.html', {'photography':photography})
+
+def search(request):
+    photographys = Photography.objects.order_by('-id').filter(isPublic=True)
+    
+    if 'search' in request.GET:
+        search_term = request.GET['search']
+        if search_term:
+            photographys = photographys.filter(name__icontains=search_term)
+
+    return render(request, 'galery/search.html', {'cards': photographys})
