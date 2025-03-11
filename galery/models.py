8cb00b498c8bb79212b12bcdbb28233f4a35cc82
GiovannaBeathryce <giovannabeathryce.pso@gmail.com>
2025-03-11 16:25:30 -0300

    Image upload configuration

--- galery/models.py
+++ galery/models.py
@@ -12,8 +12,8 @@ class Photography(models.Model):
     caption = models.CharField(max_length=150, null=False, blank=False)
     category = models.CharField(max_length=100, choices=OPTIONS_CATEGORY, default='')
     description = models.TextField(null=True, blank=True)
-    photo = models.CharField(max_length=100, null=False, blank=False)
+    photo = models.ImageField(upload_to='photos/%m/%d/', blank=True)
     isPublic = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'Photography [nome={self.name}]'
+        return self.name
