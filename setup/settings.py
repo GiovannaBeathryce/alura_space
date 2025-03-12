80c4452fe1b0d193715baf3a09d550069d00e125
GiovannaBeathryce <giovannabeathryce.pso@gmail.com>
2025-03-12 11:42:05 -0300

    starting django user app

--- setup/settings.py
+++ setup/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'galery.apps.GaleryConfig',
+    'users.app.UsersConfig'
 ]
 
 MIDDLEWARE = [
---
+++ users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+urlpatterns = [
+    # path('', index, name='home'),
+    # path('imagem/<int:photo_id>', image, name='image'),
+    # path('search', search, name='search')
+]
