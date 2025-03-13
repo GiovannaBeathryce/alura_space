cb5dc4c6b624f55a76855a055a1fe68ea528d13c
GiovannaBeathryce <giovannabeathryce.pso@gmail.com>
2025-03-13 16:17:56 -0300

    associating user and photo tables

--- galery/admin.py
+++ galery/admin.py
@@ -5,7 +5,7 @@ class ListPhotographys(admin.ModelAdmin):
     list_display = ('id', 'name', 'caption', 'isPublic')
     list_display_links = ('id', 'name')
     search_fields = ('name',)
-    list_filter = ('category',)
+    list_filter = ('category','user')
     list_editable = ('isPublic',)
 
 admin.site.register(Photography, ListPhotographys)
--- galery/models.py
+++ galery/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Photography(models.Model):
     OPTIONS_CATEGORY = [
@@ -14,6 +15,13 @@ class Photography(models.Model):
     description = models.TextField(null=True, blank=True)
     photo = models.ImageField(upload_to='photos/%m/%d/', blank=True)
     isPublic = models.BooleanField(default=False)
+    user = models.ForeignKey(
+        to = User,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=False,
+        related_name='user',
+        )
 
     def __str__(self):
         return self.name
